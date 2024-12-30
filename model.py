bd1637c15624b8a950b1435b3c9f8e69263a52c2
Sohaib9920 <sohaib9920.ahmed@gmail.com>
2024-12-30 06:45:56 +0500

    dtype

--- model.py
+++ model.py
@@ -5,11 +5,12 @@ import torch
 class ValueModel(nn.Module):
     def __init__(self, config):
         super().__init__()
+        dtype = getattr(torch, config["torch_dtype"])
         self.transformer = AutoModelForCausalLM.from_pretrained(config['lm_name'], 
-                                                                torch_dtype=getattr(torch, config["torch_dtype"]),
+                                                                torch_dtype=dtype,
                                                                 device_map=config["device_map"],
                                                                 output_hidden_states=True)
-        self.v_head = nn.Linear(self.transformer.config.hidden_size, 1)
+        self.v_head = nn.Linear(self.transformer.config.hidden_size, 1, dtype=dtype)
     
     def forward(self, *args, **kwargs):
         out = self.transformer(*args, **kwargs)
